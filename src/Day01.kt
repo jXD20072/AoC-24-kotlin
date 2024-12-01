5a434e51e613c11ebea5af1e960a2b31c8bfb34c
jXD20072 <116370539+jXD20072@users.noreply.github.com>
2024-12-01 11:25:52 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,46 @@
+import kotlin.math.abs
+
+val regex = Regex("""\d+""")
+
 fun main() {
+    fun getLists(input: List<String>): Pair<List<Int>, List<Int>> {
+        val leftList = mutableListOf<Int>()
+        val rightList = mutableListOf<Int>()
+
+        input.forEach { line ->
+            val matches = regex.findAll(line)
+            leftList.add(matches.first().value.toInt())
+            rightList.add(matches.last().value.toInt())
+        }
+
+        return Pair(leftList, rightList)
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val lists = getLists(input)
+        val leftList = lists.first.sorted()
+        val rightList = lists.second.sorted()
+
+        return leftList.foldIndexed(0) { index, acc, _ ->
+            acc + abs(rightList[index] - leftList[index])
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        val lists = getLists(input)
+        val leftList = lists.first
+        val rightList = lists.second
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+        return leftList.foldIndexed(0) {index, acc, _ ->
+            val atLeft = leftList[index]
+            acc + atLeft * rightList.count { item -> item == atLeft }
+        }
+    }
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
