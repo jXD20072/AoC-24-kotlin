de5906c22551a5bce471f3f247873962811688c5
jXD20072 <116370539+jXD20072@users.noreply.github.com>
2024-12-02 14:13:58 +0000

    Day 2 part 1

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,6 +1,20 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return 0
+        return input.count { line ->
+            val numbers = line.split(" ").map { it.toInt() }
+            val ascending = numbers[1] - numbers[0] > 0
+            
+            numbers.forEachIndexed { index, num ->
+                if (index > 0) {
+                    val difference = (num - numbers[index - 1])
+                    if (difference > 0 != ascending) return@count false
+                    if (abs(difference) < 1  || abs(difference) > 3) return@count false
+                }
+            }
+            true
+        }
     }
 
     fun part2(input: List<String>): Int {
@@ -8,7 +22,7 @@ fun main() {
     }
 
     val testInput = readInput("Day02_test")
-    check(part1(testInput) == 0)
+    check(part1(testInput) == 2)
     check(part2(testInput) == 0)
 
     val input = readInput("Day02")
