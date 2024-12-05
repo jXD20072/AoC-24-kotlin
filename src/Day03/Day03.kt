2ade50deaadbf7b817017d4cbae4dcd2c7a536f9
jXD20072 <116370539+jXD20072@users.noreply.github.com>
2024-12-05 15:38:55 +0100

    Day 3

--- src/Day03/Day03.kt
+++ src/Day03/Day03.kt
@@ -5,16 +5,44 @@ import readInput
 
 fun main() {
     fun part1(input: List<String>): Int {
-        return 0
+        val mulMatches = Regex("""mul\(\d+,\d+\)""")
+        val numbers = Regex("""\d+""")
+
+        return mulMatches.findAll(input.joinToString("")).fold(0) { acc, match ->
+            val (num1, num2) = numbers.findAll(match.value).toList().map { s -> s.value.toInt() }
+            acc + num1 * num2
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val matches = Regex("""mul\(\d+,\d+\)|do\(\)|don't\(\)""")
+        val numbers = Regex("""\d+""")
+        var enabled = true
+
+        return matches.findAll(input.joinToString("")).fold(0) { acc, match ->
+            when (match.value) {
+                "do()" -> {
+                    enabled = true
+                    acc
+                }
+                "don't()" -> {
+                    enabled = false
+                    acc
+                }
+                else -> {
+                    if (!enabled) {
+                        acc
+                    } else {
+                        val (num1, num2) = numbers.findAll(match.value).toList().map { s -> s.value.toInt() }
+                        acc + num1 * num2
+                    }
+                }
+            }
+        }
     }
 
-    val testInput = readInput("Day03_test")
-    check(part1(testInput) == 0)
-    check(part2(testInput) == 0)
+    check(part1(readInput("Day03_test1")) == 161)
+    check(part2(readInput("Day03_test2")) == 48)
 
     val input = readInput("Day03")
     part1(input).println()
